0c77be7b05cdf1d15a6219a09865d315e631f7b2
Jagadeesh Duggirala <jagadeeshchandra08@gmail.com>
2025-08-17 16:38:42 +0530

    Improved Responsiveness and Performance

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image'; 
 import { Mail, Github, Linkedin, Code, User, Book, Briefcase, ExternalLink, Globe, Star, GitFork } from 'lucide-react';
 
 const SocialLink = ({ href, icon: Icon }) => (
@@ -32,43 +33,43 @@ const LanguagesAndTools = () => (
       I use a variety of technologies to bring projects to life, including:
     </p>
     <div className="flex flex-wrap items-center justify-center sm:justify-start gap-4 p-4 rounded-lg bg-white/5 border border-white/10">
-      <a href="https://www.arduino.cc/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" className="w-10 h-10" /> </a>
-      <a href="https://aws.amazon.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/54cfe13ac10eaa1ef817a343ab0a9437eb3c2e08/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" alt="aws" className="w-10 h-10" /> </a>
-      <a href="https://azure.microsoft.com/en-in/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg" alt="azure" className="w-10 h-10" /> </a>
-      <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="bash" className="w-10 h-10" /> </a>
-      <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" className="w-10 h-10" /> </a>
-      <a href="https://www.cockroachlabs.com/product/cockroachdb/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/cockroachdb.svg" alt="cockroachdb" className="w-10 h-10" /> </a>
-      <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" className="w-10 h-10" /> </a>
-      <a href="https://www.docker.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" className="w-10 h-10" /> </a>
-      <a href="https://www.elastic.co" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/elastic/elastic-icon.svg" alt="elasticsearch" className="w-10 h-10" /> </a>
-      <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" className="w-10 h-10" /> </a>
-      <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" className="w-10 h-10" /> </a>
-      <a href="https://firebase.google.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" className="w-10 h-10" /> </a>
-      <a href="https://flask.palletsprojects.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" className="w-10 h-10" /> </a>
-      <a href="https://www.framer.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/framer/framer-icon.svg" alt="framer" className="w-10 h-10" /> </a>
-      <a href="https://cloud.google.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg" alt="gcp" className="w-10 h-10" /> </a>
-      <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" className="w-10 h-10" /> </a>
-      <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" className="w-10 h-10" /> </a>
-      <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" className="w-10 h-10" /> </a>
-      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" className="w-10 h-10" /> </a>
-      <a href="https://www.jenkins.io" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg" alt="jenkins" className="w-10 h-10" /> </a>
-      <a href="https://kubernetes.io" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/kubernetes/kubernetes-icon.svg" alt="kubernetes" className="w-10 h-10" /> </a>
-      <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" className="w-10 h-10" /> </a>
-      <a href="https://mariadb.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/54cfe13ac10eaa1ef817a343ab0a9437eb3c2e08/icons/mariadb/mariadb-original-wordmark.svg" alt="mariadb" className="w-10 h-10" /> </a>
-      <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" className="w-10 h-10" /> </a>
-      <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" className="w-10 h-10" /> </a>
-      <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" className="w-10 h-10" /> </a>
-      <a href="https://www.nginx.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" className="w-10 h-10" /> </a>
-      <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" className="w-10 h-10" /> </a>
-      <a href="https://www.oracle.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg" alt="oracle" className="w-10 h-10" /> </a>
-      <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/54cfe13ac10eaa1ef817a343ab0a9437eb3c2e08/icons/photoshop/photoshop-plain.svg" alt="photoshop" className="w-10 h-10" /> </a>
-      <a href="https://www.php.net" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" className="w-10 h-10" /> </a>
-      <a href="https://postman.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" className="w-10 h-10" /> </a>
-      <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" className="w-10 h-10" /> </a>
-      <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" className="w-10 h-10" /> </a>
-      <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" className="w-10 h-10" /> </a>
-      <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" className="w-10 h-10" /> </a>
-      <a href="https://vuejs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg" alt="vuejs" className="w-10 h-10" /> </a>
+      <a href="https://www.arduino.cc/" target="_blank" rel="noreferrer"> <Image src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" width={40} height={40} /> </a>
+      <a href="https://aws.amazon.com" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/54cfe13ac10eaa1ef817a343ab0a9437eb3c2e08/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" alt="aws" width={40} height={40} /> </a>
+      <a href="https://azure.microsoft.com/en-in/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg" alt="azure" width={40} height={40} /> </a>
+      <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="bash" width={40} height={40} /> </a>
+      <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width={40} height={40} /> </a>
+      <a href="https://www.cockroachlabs.com/product/cockroachdb/" target="_blank" rel="noreferrer"> <Image src="https://cdn.worldvectorlogo.com/logos/cockroachdb.svg" alt="cockroachdb" width={40} height={40} /> </a>
+      <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width={40} height={40} /> </a>
+      <a href="https://www.docker.com/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width={40} height={40} /> </a>
+      <a href="https://www.elastic.co" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/elastic/elastic-icon.svg" alt="elasticsearch" width={40} height={40} /> </a>
+      <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width={40} height={40} /> </a>
+      <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width={40} height={40} /> </a>
+      <a href="https://firebase.google.com/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width={40} height={40} /> </a>
+      <a href="https://flask.palletsprojects.com/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" width={40} height={40} /> </a>
+      <a href="https://www.framer.com/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/framer/framer-icon.svg" alt="framer" width={40} height={40} /> </a>
+      <a href="https://cloud.google.com" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg" alt="gcp" width={40} height={40} /> </a>
+      <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width={40} height={40} /> </a>
+      <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width={40} height={40} /> </a>
+      <a href="https://www.java.com" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width={40} height={40} /> </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width={40} height={40} /> </a>
+      <a href="https://www.jenkins.io" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg" alt="jenkins" width={40} height={40} /> </a>
+      <a href="https://kubernetes.io" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/kubernetes/kubernetes-icon.svg" alt="kubernetes" width={40} height={40} /> </a>
+      <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width={40} height={40} /> </a>
+      <a href="https://mariadb.org/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/54cfe13ac10eaa1ef817a343ab0a9437eb3c2e08/icons/mariadb/mariadb-original-wordmark.svg" alt="mariadb" width={40} height={40} /> </a>
+      <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width={40} height={40} /> </a>
+      <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width={40} height={40} /> </a>
+      <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <Image src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" width={40} height={40} /> </a>
+      <a href="https://www.nginx.com" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width={40} height={40} /> </a>
+      <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width={40} height={40} /> </a>
+      <a href="https://www.oracle.com/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg" alt="oracle" width={40} height={40} /> </a>
+      <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/54cfe13ac10eaa1ef817a343ab0a9437eb3c2e08/icons/photoshop/photoshop-plain.svg" alt="photoshop" width={40} height={40} /> </a>
+      <a href="https://www.php.net" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width={40} height={40} /> </a>
+      <a href="https://postman.com" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width={40} height={40} /> </a>
+      <a href="https://www.python.org" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width={40} height={40} /> </a>
+      <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width={40} height={40} /> </a>
+      <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> <Image src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width={40} height={40} /> </a>
+      <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width={40} height={40} /> </a>
+      <a href="https://vuejs.org/" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg" alt="vuejs" width={40} height={40} /> </a>
     </div>
   </Card>
 );
@@ -261,24 +262,28 @@ export default function App() {
       </style>
       
       <div className="relative z-10">
-        <div className="flex flex-col items-center justify-center min-h-screen p-4 text-center ">
-          <div className="flex flex-col items-center justify-center h-full space-y-6 ">
+        <div className="flex flex-col items-center justify-center min-h-screen p-4 text-center">
+          <div className="flex flex-col items-center justify-center h-full space-y-6">
             <h1 className="text-4xl md:text-5xl font-semibold text-white tracking-wide">Jagadeesh Chandra Duggirala</h1>
             <h2 className="text-lg md:text-xl text-gray-300 mt-2 tracking-wide">Cybersecurity & Photography</h2>
             <div className="flex space-x-6 mt-10">
               <SocialLink href="mailto:jagadeesh.duggirala22@st.niituniversity.in" icon={Mail} />
               <SocialLink href="https://github.com/MrCrafter32" icon={Github} />
-              <SocialLink href="https://linkedin.com/in/jagadeesh-chandra-duggirala" icon={Linkedin} />
+              <SocialLink href="www.linkedin.com/in/jagadeesh-chandra-duggirala-8aa715251" icon={Linkedin} />
               <SocialLink href="https://mrcrafter.tech" icon={Code} />
             </div>
-            <nav className="sticky top-4 bg-white/10 backdrop-blur-lg z-30 py-4 mb-16 border border-white/20 transition-colors duration-300 rounded-2xl shadow-md w-3xl">
-            <ul className="flex justify-center space-x-4 md:space-x-6">
+          </div>
+        </div>
+
+        <main className="container mx-auto px-4 py-8 max-w-4xl">
+          <nav className="sticky top-4 bg-white/10 backdrop-blur-lg z-30 py-4 mb-16 border border-white/20 transition-colors duration-300 rounded-2xl shadow-md">
+            <ul className="flex justify-center space-x-2 md:space-x-6">
               {navItems.map((item) => (
                 <li key={item.sectionId}>
                   <a
                     href={`#${item.sectionId}`}
                     onClick={(e) => { e.preventDefault(); scrollToSection(item.sectionId); }}
-                    className="font-medium text-gray-200 hover:text-white transition-colors duration-200 px-3 py-2 rounded-md"
+                    className="font-medium text-gray-200 hover:text-white transition-colors duration-200 px-2 py-2 md:px-3 rounded-md text-sm md:text-base"
                   >
                     {item.name}
                   </a>
@@ -286,10 +291,6 @@ export default function App() {
               ))}
             </ul>
           </nav>
-          </div>
-        </div>
-
-        <main className="container mx-auto px-4 py-8 max-w-4xl">
           
           <div className="space-y-24">
             <section id="about" className="scroll-mt-24">
@@ -309,7 +310,8 @@ export default function App() {
             </section>
           </div>
         </main>
-
+        
+        
       </div>
     </div>
   );
